219a49f4eec1ddaa4a786f652b412b7a66b7ed0d
arafatalawy <151236616+arafatalawy@users.noreply.github.com>
2023-11-20 22:54:00 +0300

    Update test_container.ps1

--- scripts/test_container.ps1
+++ scripts/test_container.ps1
@@ -1,17 +1,17 @@
 $count =0
 do {
-$count++
+   $count++
 
-write-output "[$env:STAGE_NAME] starting container[Attempt: $count]"
-$teststart = Invoke-WebRequest -Uri http://localhost:8081
-if ($teststart.statuscode -eq '200')
-     $started = $true
-}
-else
-{
-    start-sleep -Seconds 1
-}
+   write-output "[$env:STAGE_NAME] starting container[Attempt: $count]"
+   $teststart = Invoke-WebRequest -Uri http://localhost:8081
+   if ($teststart.statuscode -eq '200')
+     { $started = $true
+        }
+   else
+   {
+      start-sleep -Seconds 1
+    }
 } until (!$started -or ($count -eq 3) )
-if (!$started)
+if (!$started){
    exit 1
 }
